5ea95a585bcc2872921e9652ecd7e68969288bb2
anzalass <anzalas.muhammad@gmail.com>
2024-07-31 18:47:14 +0700

    7-31.2

--- src/App.jsx
+++ src/App.jsx
@@ -84,7 +84,7 @@ function App() {
           />
           <Route
             path="/detail-pengadaan/:id"
-            element={<DetailPengadaanPage />}
+            element={isLogin ? <DetailPengadaanPage /> : <LoginPage />}
           />
           <Route
             path="/invoice/:id"
