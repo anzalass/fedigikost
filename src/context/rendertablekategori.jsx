16e3893f3ed59e79f7c5ac337e62bcfc12ed4355
Muhamad Gempar Anzalas <109114576+anzalass@users.noreply.github.com>
2024-09-03 12:56:19 +0700

    test

--- src/context/rendertablekategori.jsx
+++ src/context/rendertablekategori.jsx
@@ -3,7 +3,7 @@ import { createContext, useContext, useState } from "react";
 const RenderTableKategori = createContext();
 
 const RenderTableKategoriContext = ({ children }) => {
-  const [renderkategori, setRenderKategori] = useState(false);
+  const [renderkategori, setRenderKategori] = useState(false)
 
   return (
     <RenderTableKategori.Provider value={[renderkategori, setRenderKategori]}>
